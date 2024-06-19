4fea4dcfa1fef480d2fb98f5fe98a8a07742d602
Komborerai Chikweshe <kombogc@gmail.com>
2024-06-19 14:35:32 +0200

    initial

--- models.py
+++ models.py
@@ -8,43 +8,6 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 Base = database.Base
 
 
-class Movies(Base):
-    __tablename__ = "movies"
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    movie_name = Column(String(20))
-    actor_id = Column(Integer, ForeignKey("actors.id"))
-    director_id = Column(Integer, ForeignKey("directors.id"))
-    genre_id = Column(Integer, ForeignKey("genre.id"))
-    rating = Column(Integer)
-    release_date = Column(String(60))
-    is_active = Column(Boolean, default=True)
-    actor = relationship("Actor")
-    director = relationship("Director")
-    director = relationship("Genre")
-
-
-class Actor(Base):
-    __tablename__ = "actors"
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    actor_name = Column(String(20))
-    is_active = Column(Boolean, default=True)
-
-
-class Genre(Base):
-    __tablename__ = "genre"
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    genre_name = Column(String(20))
-    is_active = Column(Boolean, default=True)
-
-
-class Director(Base):
-    __tablename__ = "directors"
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    director_name = Column(String(20))
-    is_active = Column(Boolean, default=True)
-
-
-
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
